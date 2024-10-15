a0a5fffd74a1a2671104fd8821d02ce9896ec9b4
reyhane <97376622+reyhane1376@users.noreply.github.com>
2024-10-15 13:04:31 +0330

    add jwt service

---
+++ app/Services/JwtService.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Servicesp;
+
+use Firebase\JWT\JWT;
+use Firebase\JWT\Key;
+
+class JwtService {
+    private $secretKey;
+
+    public function __construct()
+    {
+        $this->secretKey = env('JWT_SECRET');
+    }
+
+    public function generateToken()
+    {
+        $payload = [
+            'iss' => 'http://example.org',
+            'aud' => 'http://example.com',
+            'iat' => 1356999524,
+            'nbf' => 1357000000
+        ];
+        
+        $jwt = JWT::encode($payload, $this->secretKey, 'HS256');
+
+        return $jwt;
+    }
+
+    public function decodeToken($token)
+    {
+        try {
+            return JWT::decode($token, new Key($this->secretKey, 'HS256'));
+        } catch (\Exception $e) {
+            return null;
+        }
+    }
+}
